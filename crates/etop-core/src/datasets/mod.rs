60eb2d66c63344c1fd1ddb919de59a1f912974bb
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-12-01 13:45:22 -0800

    Update mod.rs

--- crates/etop-core/src/datasets/mod.rs
+++ crates/etop-core/src/datasets/mod.rs
@@ -1,9 +1,10 @@
 mod blocks;
 mod cryo_dataset;
 mod erc20_transfers_by_erc20;
+mod erc721;
 mod transactions_by_to_address;
-mod erc721_transfer_by_erc721;
 pub use blocks::Blocks;
 pub use cryo_dataset::CryoDataset;
 pub use erc20_transfers_by_erc20::Erc20TransfersByErc20;
+pub use erc721::Erc721TransfersByErc721;
 pub use transactions_by_to_address::TransactionsByToAddress;
